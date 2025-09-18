c0f7da961d51899b14cd4c596852a4e3ebd29cb0
Richmond Cowther <richmondcowther@gmail.com>
2025-09-18 22:18:01 +0000

    Add files via upload

---
+++ alx-project-0x00/components/Button.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { ButtonProps } from "@/interfaces";
+
+const Button: React.FC<ButtonProps> = ({
+  title = "Default Button", 
+  style = "",               
+  size = "medium",         
+  shape = "rounded-md"      
+}) => {
+  const sizeClasses = {
+    small: "px-2 py-1 text-sm",
+    medium: "px-4 py-2 text-base",
+    large: "px-6 py-3 text-lg",
+  };
+
+  return (
+    <button
+      className={`bg-blue-500 text-white hover:bg-blue-600 ${sizeClasses[size]} ${shape} ${style}`}
+    >
+      {title}
+    </button>
+  );
+};
+
+export default Button;
--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,15 +1,16 @@
 import Card from "@/components/Card";
+import Button from "@/components/Button";
 
 const Landing: React.FC =  () => {
   return (
     <div>
-      <h1 className=" text-xl font-extralight">
-        Landing Page
-      </h1>
-
+      <h1 className=" text-xl font-extralight">Landing Page</h1>
+      
       <Card />
+      <Card />
+      
+     <Button title="Click Me" size="medium" shape="rounded-md" style="extra-classes" />
     </div>
   )
 }
-
 export default Landing;
